b3d3db2542607e5092be0055d0916739a8aea845
leezed <975500562@qq.com>
2023-01-24 18:13:04 +0800

    init

---
+++ src/main/java/com/lee/yiqun/YiqunApplication.java
@@ -0,0 +1,13 @@
+package com.lee.yiqun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiqunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiqunApplication.class, args);
+    }
+
+}
